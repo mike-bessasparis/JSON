e464504912b143899d3e1609b6d95e885b9f5825
Michael Bessasparis <michael.bessasparis@gmail.com>
2015-04-11 18:33:08 -0500

    added answer buttons

--- app/src/main/java/com/bessasparis/mike/json/MainActivity.java
+++ app/src/main/java/com/bessasparis/mike/json/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -16,20 +17,35 @@ import java.io.InputStream;
 
 public class MainActivity extends ActionBarActivity {
 
+    public String jsonString;
+    public JSONObject jsonObj;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        updateUI();
+        try {
+            jsonString = loadJSONFromAsset();
+            jsonObj = new JSONObject(jsonString);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        updateUI(jsonObj);
     }
 
-    public void updateUI() {
-        String mStr = loadJSONFromAsset();
+    public void updateUI(JSONObject mObj) {
+        TextView questionTextView = (TextView) findViewById(R.id.question);
+        Button choice1 = (Button) findViewById(R.id.answerchoice1);
+        Button choice2 = (Button) findViewById(R.id.answerchoice2);
+        Button choice3 = (Button) findViewById(R.id.answerchoice3);
+        Button choice4 = (Button) findViewById(R.id.answerchoice4);
 
         try {
-            Log.i("mjb", "Q1: " + getQuestionText(mStr, 0));
-            TextView questionTextView = (TextView) findViewById(R.id.question);
-            questionTextView.setText(getQuestionText(mStr, 0));
+            questionTextView.setText(getQuestionText(mObj, 0));
+            choice1.setText(getChoiceText(mObj, 0, 1));
+            choice2.setText(getChoiceText(mObj, 0, 2));
+            choice3.setText(getChoiceText(mObj, 0, 3));
+            choice4.setText(getChoiceText(mObj, 0, 4));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -37,14 +53,38 @@ public class MainActivity extends ActionBarActivity {
 
 
 
-    public String getQuestionText(String questionJsonStr, int i) throws JSONException {
+    public String getQuestionText(JSONObject mObj, int i) throws JSONException {
         String questionText;
 
-        JSONObject mObj = new JSONObject(questionJsonStr);
         JSONArray questionsArray = mObj.getJSONArray("questions");
         JSONObject questionObj = questionsArray.getJSONObject(i);
         questionText = questionObj.getString("questiontext");
         return questionText;
+    }
+
+    // i is index to question
+    // j is which answerchoice to get
+    public String getChoiceText(JSONObject mObj, int i, int j) throws JSONException {
+        String choiceText = null;
+
+        JSONArray questionsArray = mObj.getJSONArray("questions");
+        JSONObject questionObj = questionsArray.getJSONObject(i);
+
+        switch(j) {
+            case 1:
+                choiceText = questionObj.getString("answerchoice1");
+                break;
+            case 2:
+                choiceText = questionObj.getString("answerchoice2");
+                break;
+            case 3:
+                choiceText = questionObj.getString("answerchoice3");
+                break;
+            case 4:
+                choiceText = questionObj.getString("answerchoice4");
+                break;
+        }
+        return choiceText;
     }
 
 
